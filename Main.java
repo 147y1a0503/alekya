972369a1c9c041aec54952a02a0b959373e17fe1
147y1a0503 <31848982+147y1a0503@users.noreply.github.com>
2017-09-13 13:26:46 +0530

    my first commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+	Student st=new Student(555,"raja rao",12-04-1996,5600.00);
+	Student st1=new Student(556,"raju reddy",20-08-1997,5000.000);
+	st.hashcode();
+	st.equals(st1);
+	st.compare(st10);
+	
+							//You may test that your code works find here
+			//Please check that your code works and has no 
+			//compilation problems before to submit
+	}
+
+}
